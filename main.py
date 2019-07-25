4ca4e857ebf2c89173a9bdba8432fb4768f2af79
sporter399 <sjporter399@gmail.com>
2019-07-25 11:38:39 -0600

    1138am commit of 7/25 blogz

--- main.py
+++ main.py
@@ -7,9 +7,21 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 project_dir = os.path.dirname(os.path.abspath(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir, "build-a-blog.db"))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///{}".format(os.path.join(project_dir, "blogz.db"))
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.password = password
 
 
 class Blog(db.Model):
@@ -17,27 +29,101 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     blog = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
    
     
-    def __init__(self, title, blog):
+    def __init__(self, title, blog, owner):
        self.title = title
        self.blog = blog
+       self.owner = owner
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+      
+      if request.method == 'POST':
+          user_name = request.form['user_name']
+          password = request.form['password']
+          verify = request.form['verify']
+          user_name_db_count = User.query.filter_by(user_name=user_name).count()
+          if user_name_db_count > 0:
+              flash('yikes! "' + user_name + '" is already taken and password reminders are not implemented')
+              return redirect('/signup')
+          if password != verify:
+              flash('passwords did not match')
+              return redirect('/signup')
+          if (len(user_name) <= 3) or (user_name.strip() == ""):
+              flash("User names must contain at least 3 characters.")
+              return redirect('/signup')
+        
+          if (len(password) <= 3) or (password.strip() == ""):
+              flash("Passwords must contain at least 3 characters.")
+              return redirect('/signup')
+        
+          user = User(user_name=user_name, password=password)
+          db.session.add(user, password)
+          db.session.commit()
+          session['user'] = user.user_name
+          return redirect("/")
+      """
+            else:
+                  return render_template('signup.html')
+      """
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+      if request.method == 'POST':
+          user_name = request.form['user_name']
+          password = request.form['password']
+          user = User.query.filter_by(user_name=user_name).first()
+
+          if user_name != user:
+              flash('Please register for an account to begin blogging.')
+              return redirect('/signup')
+      
+          if password != password:
+              flash('Passwords do not match')
+              return redirect('/signup')
+            
+          if user and user.password == password:
+              session['user_name'] = user_name
+              flash("Logged in")
+              return redirect('/addconfirm')
+       
+      return render_template('login.html')
+"""
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+      return
+"""
+
+@app.route('/logout', methods=['POST', 'GET'])
+def logout():
+
+      return redirect('/blog.html')
 
 
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
+
+      
      
       return render_template('blogenter.html',title="Title for your new blog:", blog="Your blog")       
 
 @app.route('/addconfirm', methods=['POST', 'GET'])
 def addconfirm():
+
+      #app must acquire identity of a logged in user here as they enter their blog
+      owner = User.query.filter_by(user_name=session['user_name']).first()
       
       if request.method == 'POST':
        
         title = request.form['title']
         blog = request.form['blog'] 
-        new_blog_object = Blog(title, blog)
+        new_blog_object = Blog(title, blog, owner)
         db.session.add(new_blog_object)      
         db.session.commit()
 
